352bcdfc51059c0aa7d6f0ae173ed57816f5d0a4
Pacy <40542052+pacy97@users.noreply.github.com>
2018-07-07 00:13:55 +0530

    Update ImageView.java

--- ImageView.java
+++ ImageView.java
@@ -1,1 +1,33 @@
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+/**
+ * Created by sunith on 16-10-2017.
+ */
+
+public class ImageView extends AppCompatActivity  {
+    ImageView imv;
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startpage);
+        imv = (ImageView) findViewById(R.id.imageview);
+        imv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                  Toast.makeText(ImageView.this, "You have Clicked the ImageView", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+}
